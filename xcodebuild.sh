3787fa089e65b28cf706a71854be3311d5a6fcbd
Berstanio <berstaniodev@gmail.com>
2023-06-27 15:51:40 +0200

    Build arm64 simulator, build libpref, simplify xcodeproj

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -1,21 +1,36 @@
+rm -r xcode
 mkdir -p xcode
 
 buildIos() {
   echo building for iOS
-  xcodebuild -sdk iphoneos -arch arm64 -project jdk-ios.xcodeproj -scheme jdk-arm64-debug   > xcode/jdk-arm64-d.log 2>&1
-  xcodebuild -sdk iphoneos -arch arm64 -project jdk-ios.xcodeproj -scheme jdk-arm64-release > xcode/jdk-arm64-r.log 2>&1
+  xcodebuild -sdk iphoneos -arch arm64 -project jdk-ios.xcodeproj -target jdk -configuration Debug  > xcode/jdk-iphoneos-d.log 2>&1
+  xcodebuild -sdk iphoneos -arch arm64 -project jdk-ios.xcodeproj -target jdk -configuration Release > xcode/jdk-iphoneos-r.log 2>&1
 
-  lipo -info xcode/jdk-arm64-ios-d.a
-  lipo -info xcode/jdk-arm64-ios-r.a
+  lipo -info xcode/jdk-ios-d.a
+  lipo -info xcode/jdk-ios-r.a
 }
 
 buildIosSimulator() {
   echo building for iOS Simulator
-  xcodebuild -sdk iphonesimulator -arch x86_64 -project jdk-ios.xcodeproj -scheme jdk-x86-64-debug   > xcode/x86-64-ios-simulator-d.log 2>&1
-  xcodebuild -sdk iphonesimulator -arch x86_64 -project jdk-ios.xcodeproj -scheme jdk-x86-64-release > xcode/x86-64-ios-simulator-r.log 2>&1
+  xcodebuild -sdk iphonesimulator -arch x86_64 -project jdk-ios.xcodeproj -target jdk -configuration Release  > xcode/jdk-iphonesimulator-x86_64-r.log 2>&1
+  xcodebuild -sdk iphonesimulator -arch x86_64 -project jdk-ios.xcodeproj -target jdk -configuration Debug  > xcode/jdk-iphonesimulator-x86_64-d.log 2>&1
+  
+  mv xcode/jdk-ios-simulator-d.a xcode/jdk-ios-simulator-x86_64-d.a
+  mv xcode/jdk-ios-simulator-r.a xcode/jdk-ios-simulator-x86_64-r.a
 
-  lipo -info xcode/jdk-x86-64-ios-simulator-d.a
-  lipo -info xcode/jdk-x86-64-ios-simulator-r.a
+  xcodebuild -sdk iphonesimulator -arch arm64 -project jdk-ios.xcodeproj -target jdk -configuration Release > xcode/jdk-iphonesimulator-arm64-r.log 2>&1
+  xcodebuild -sdk iphonesimulator -arch arm64 -project jdk-ios.xcodeproj -target jdk -configuration Debug  > xcode/jdk-iphonesimulator-arm64-d.log 2>&1
+
+  mv xcode/jdk-ios-simulator-d.a xcode/jdk-ios-simulator-arm64-d.a
+  mv xcode/jdk-ios-simulator-r.a xcode/jdk-ios-simulator-arm64-r.a
+
+  lipo xcode/jdk-ios-simulator-arm64-d.a xcode/jdk-ios-simulator-x86_64-d.a -create -output xcode/jdk-ios-simulator-d.a
+  lipo xcode/jdk-ios-simulator-arm64-r.a xcode/jdk-ios-simulator-x86_64-r.a -create -output xcode/jdk-ios-simulator-r.a
+
+  rm xcode/jdk-ios-simulator-arm64-d.a xcode/jdk-ios-simulator-x86_64-d.a xcode/jdk-ios-simulator-arm64-r.a xcode/jdk-ios-simulator-x86_64-r.a
+
+  lipo -info xcode/jdk-ios-simulator-d.a
+  lipo -info xcode/jdk-ios-simulator-r.a
 }
 
 # Mac Catalyst
